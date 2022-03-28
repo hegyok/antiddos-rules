2509df61af29249d87db5eedd298b6d4369fb7cc
Hegy <45619085+Heges69@users.noreply.github.com>
2022-03-28 13:50:29 +0200

    Create antiddos.sh

---
+++ antiddos.sh
@@ -0,0 +1,62 @@
+#hod do pice invalid
+iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+
+#opdjebat packety co jsou nove a nejsou SYN
+iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+
+#tohle jsem nekde skidnul
+iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+
+#tohle taky
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP 
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+
+#odjeb private subnety aby to nikdo nespoofoval lol
+iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP 
+iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP 
+iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP 
+iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP 
+iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP 
+iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP 
+iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP 
+iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP 
+iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+
+#imagine používat ICMP
+iptables -t mangle -A PREROUTING -p icmp -j DROP
+iptables -A INPUT -p tcp -m connlimit --connlimit-above 80 -j REJECT --reject-with tcp-reset
+
+#DDoS ble
+iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
+iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+iptables -t mangle -A PREROUTING -f -j DROP
+
+#useless hovno 
+iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT 
+iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+
+#SYN flood je gay
+iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack 
+iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460 
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+
+#nebudeš port scanovat zmrde
+iptables -N port-scanning 
+iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN 
+iptables -A port-scanning -j DROP
+
+#antibruteforce ssh
+iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --set 
+iptables -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
